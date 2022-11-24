d344af41f74157004f492d64cee74c2b89d62aca
NickGMeade <90179901+NickGMeade@users.noreply.github.com>
2022-11-24 02:55:38 +0100

    Added refactored files
    scales.py contains the refactored example.py code for this project, main.py is the file for activating the scale and run the code.

---
+++ main.py
@@ -0,0 +1,9 @@
+from hx711.example import *
+from hx711.emulated_hx711 import *
+from hx711.hx711 import *
+from hx711.setup import *
+from hx711.scales import *
+
+scaleInfo = initialiseScale()
+
+checkWeight(scaleInfo)
